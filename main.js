ac68dff1af501e8e8e8447772ab0487c6715c8ff
Christoffer Skytte Wielsøe <FatherofPiper@Marys-MacBook-Pro.local>
2019-09-10 10:47:09 +0200

    Data Fetch succesful

--- main.js
+++ main.js
@@ -15,6 +15,19 @@ function showData(data){
 function showDoggos(doggoData) {
      console.log(doggoData)
 
+
+    const template = document.querySelector("template").content;
+    const myCopy = template.cloneNode(true);
+
+
+    myCopy.querySelector(".breed").textContent = doggoData.gsx$breed.$t;
+    myCopy.querySelector(".name").textContent = doggoData.gsx$name.$t;
+    myCopy.querySelector(".sex").textContent = doggoData.gsx$sex.$t;
+    myCopy.querySelector(".shortD").textContent = doggoData.gsx$shortdescription.$t;
+    myCopy.querySelector(".longD").textContent = doggoData.gsx$longdescription.$t;
+    myCopy.querySelector(".childFriend").textContent = "Child Friendly: " + doggoData.gsx$childfriendly.$t;
+    myCopy.querySelector(".category").textContent = doggoData.gsx$category.$t;
+
     document.querySelector("main").appendChild(myCopy)
 
 }
