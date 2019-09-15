1543f2072e823954ea4e9d8ae34914b37ae57294
Christoffer Wielsøe <fatherofpiper@Christoffers-iMac.local>
2019-09-15 09:46:10 +0200

    Explanation added to basic JS

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+// Define our data from Google Spreadsheets /list/OUR-LINK/od6
 const link = "https://spreadsheets.google.com/feeds/list/1qABL59AhMRLBAoIg8XRuN3Aax98QuyqUxZ1IIADjBLQ/od6/public/values?alt=json";
 window.addEventListener("load", getData);
+
+// Fetch our Data
 
 function getData() {
     fetch(link)
@@ -12,11 +15,14 @@ function showData(data) {
     myArray.forEach(showDoggos);
 }
 
+
 function showDoggos(doggoData) {
     console.log(doggoData)
 
+// Using a local img folder with names from Spreadsheet
     let imageName = "assets/imgs/dogs/" + doggoData.gsx$img.$t + ".jpg";
 
+// Calculate years, months and days in
     let today = new Date();
     let timeRescued = new Date(doggoData.gsx$timein.$t);
     var diff_date = today - timeRescued;
@@ -25,10 +31,11 @@ function showDoggos(doggoData) {
     var num_months = (diff_date % 31536000000) / 2628000000;
     var num_days = ((diff_date % 31536000000) % 2628000000) / 86400000;
 
+// Defines templete and create a copy
     const template = document.querySelector("template").content;
     const myCopy = template.cloneNode(true);
 
-
+// Inputs data to cards
     myCopy.querySelector(".data-name").textContent = doggoData.gsx$name.$t;
     myCopy.querySelector(".data-breed").textContent = doggoData.gsx$breed.$t;
     myCopy.querySelector(".data-category").textContent = doggoData.gsx$category.$t;
@@ -37,7 +44,7 @@ function showDoggos(doggoData) {
 
 
 
-
+// Appends cards to main
     document.querySelector("main").appendChild(myCopy)
 
 }
